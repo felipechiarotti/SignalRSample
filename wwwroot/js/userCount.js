756b20c4c2771fc279d324d7e6bd48313307878b
felipechiarotti <felipe.giron@optiisolutions.com>
2023-07-17 18:40:40 -0300

    SignalR added and configured on client and server side

---
+++ wwwroot/js/userCount.js
@@ -0,0 +1,30 @@
+﻿// Create Connection
+var connectionUserCount = new signalR.HubConnectionBuilder().withUrl("/hubs/userCount").build();
+
+// Connect to methods
+connectionUserCount.on("updateTotalViews", (value) => {
+    var newCountSpan = document.getElementById("totalViewsCounter");
+    newCountSpan.innerText = value.toString();
+});
+
+connectionUserCount.on("updateTotalUsers", (value) => {
+    var newCountSpan = document.getElementById("totalUsersCounter");
+    newCountSpan.innerText = value.toString();
+});
+
+// Invoke Method
+function newWindowLoadedOnClient() {
+    connectionUserCount.send("NewWindowLoaded");
+}
+
+// Start Connection
+function success() {
+    console.log("Connection to SignalR successful");
+    newWindowLoadedOnClient();
+}
+
+function rejected() {
+    console.log("Connection to SignalR rejected");
+}
+
+connectionUserCount.start().then(success, rejected);
